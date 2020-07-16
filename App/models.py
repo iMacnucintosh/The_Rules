a14db8ae32ddfd9d85315b8084c3145051a0f299
Manuel <iMacnucintosh@icloud.com>
2020-07-16 15:02:03 +0200

    Listado de reglas con scroll

--- App/models.py
+++ App/models.py
@@ -3,21 +3,18 @@ import os
 
 from django.db import models
 
-def path_upload_rule(filename):
+def path_upload_rule(instance, filename):
     var = filename.split('.')
     var_length = len(var) - 1
     var_extension = var[var_length]
 
     num_rules = len(Rule.objects.all())
-
-    return "static/images/rules/" + str(num_rules + 1) + "." + var_extension
-
+    return "static/images/rules/" + str(num_rules + 1) + "." + var_extension.lower()
 
 class Rule(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.FileField(upload_to=path_upload_rule, blank=True, null=True)
-    JSFunction = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return str(self.id) + " - " + self.name
