18b8d4d9c2c8f03eec42d5af74852c7fa56784c5
Manuel López <iMacnucintosh@icloud.com>
2020-08-08 21:40:59 +0200

    Update Fixed

--- static/js/general_functions.js
+++ static/js/general_functions.js
@@ -1,5 +1,12 @@
 var image_rule;
+var last_rule = null;
+var bebidas = ["Larios", "Brugal", "Barcelo", "Whisky", "Cerveza", "Calimocho", "Vodka"];
+var colores = [{"name": "Blanco", "code": "white"},{"name": "Negro", "code": "black"}, {"name": "Rojo", "code": "red"}, {"name": "Azul", "code": "blue"}, {"name": "Rojo", "code": "red"}, {"name": "Marron", "code": "brown"}, {"name": "Rosa", "code": "pink"}, {"name": "Verde", "code": "green"}, {"name": "Morado", "code": "purple"}];
+
 function nextRule(){
+
+    // Reseteo de reglas anteriores
+    $('.txt-rule').remove();
 
     var active_rules = [];
 
@@ -19,13 +26,21 @@ function nextRule(){
             }
         });
     }else{
-        var random_rule = active_rules[Math.floor((Math.random() * active_rules.length))];
-        image_rule = $('#' + random_rule).find(".rule-image").attr("style");
+
+        var random_number = Math.floor((Math.random() * active_rules.length));
+
+        while(random_number == last_rule){
+            random_number = Math.floor((Math.random() * active_rules.length));
+        }
+
+        var random_rule = active_rules[random_number];
+        image_rule = $('#' + random_rule).find(".rule-image").attr("src");
+
         name_rule = $('#' + random_rule).find(".rule-info h1").text();
 
         $('.active_rule').addClass("fade-rule");
 
-        $('.active_rule').attr("style", image_rule);
+        $('.active_rule').attr("style", "background-image: url(" + image_rule + ")");
 
         $('.active_rule').removeClass("fade-rule");
 
@@ -33,24 +48,42 @@ function nextRule(){
 
         checkSpecialRule(parseInt(random_rule.split("_")[1]));
 
+        last_rule = random_number;
+
     }
 }
 
 function checkSpecialRule(id){
     switch(id){
-        case 14:
+        case 14: // Ruleta Rusa
             $('.bg-next-rule').removeAttr("onclick");
             $('.active_rule').append('<div class="shoot"></div>');
+
+            var shootON = document.getElementById("shootON");
+            var shootOFF = document.getElementById("shootOFF");
 
             var players = parseInt($('.player-count').text());
             var shoots = 1;
 
             $('.shoot').click(function(){
+
+                if($('.toast').length >= 2){
+                    $('.toast')[0].remove();
+                }
+
+                shootON.pause()
+                shootON.currentTime=0
+
+                shootOFF.pause()
+                shootOFF.currentTime=0
+
                 var shoot = Math.round(Math.random())
                 if(shoot == 0){
-                    M.toast({html: 'Jugador ' + shoots + ' Libras'}, 500)
+                    M.toast({html: 'Jugador ' + shoots + ' Libras'}, 500);
+                    shootOFF.play();
                 }else{
-                    M.toast({html: 'Jugador ' + shoots + ' ¡BEBES!', classes: 'red'}, 500)
+                    M.toast({html: 'Jugador ' + shoots + ' ¡BEBES!', classes: 'red'}, 500);
+                    shootON.play();
                 }
 
                 if(shoots == players){
@@ -59,6 +92,20 @@ function checkSpecialRule(id){
                 }
                 shoots++;
             });
+
+            break;
+        case 23: // Adictos a una Bebida
+
+            var bebida = bebidas[Math.floor((Math.random() * bebidas.length))]
+
+            $('.current-rule').append("<h1 class='txt-rule'>" + bebida + "</h1>")
+
+            break;
+         case 27: // Ropajes de Color
+
+            var color = colores[Math.floor((Math.random() * colores.length))]
+
+            $('.current-rule').append("<h1 class='txt-rule' style='color:" + color.code + "'>" + color.name + "</h1>")
 
             break;
     }
--- static/js/startapp.js
+++ static/js/startapp.js
@@ -5,12 +5,29 @@
 $(document).ready(function(){
     M.AutoInit();
 
+    // Cargamos las reglas seleccionadas anteriormente
+    if(localStorage.getItem("rules") != undefined){
+        var active_rules = JSON.parse(localStorage.getItem("rules"));
+
+        for(i=0; i<active_rules.length; i++){
+            console.log(i);
+            $('#rule_' + active_rules[i]).find('input').prop("checked", "checked");
+        }
+
+    }
+
+    if(localStorage.getItem("players") != undefined){
+         $('.player-count').text(localStorage.getItem("players"));
+    }
+
     $('.left-top.corner').click(function(){
         $('.rules').addClass('active')
     });
 
     $('.right-top.corner').click(function(){
         $('.game').removeClass("active");
+         $('.corner').removeClass("corner-black");
+         $('.btn-info-partida').fadeOut();
     });
 
     $('.close-rules').click(function(){
@@ -22,47 +39,54 @@ $(document).ready(function(){
 
         if(current_players >= 3){
             $('.player-count').text(current_players - 1);
+            localStorage.setItem("players", current_players - 1)
         }
     });
 
     $('.add-player').click(function(){
         var current_players = parseInt($('.player-count').text())
         $('.player-count').text(current_players + 1);
+        localStorage.setItem("players", current_players + 1)
     });
 
     $('.start-game > article').click(function(){
         $('.game').addClass('active');
+        $('.corner').addClass("corner-black");
+        $('.btn-info-partida').css("display", "flex").hide().fadeIn();
     });
 
-    // $('.switch').find("input").change(function(){
-    //     var value = $(this).prop("checked");
-    //     var id = $(this).attr("id").split("_")[1];
-    //
-    //     if(localStorage.getItem("rules") == undefined){
-    //         localStorage.setItem("rules", JSON.stringify([]))
-    //     }
-    //
-    //     var active_rules = JSON.parse(localStorage.getItem("rules"));
-    //
-    //     var exists = false;
-    //
-    //     for(var i=0; i<active_rules.length; i++){
-    //         if(parseInt($(this).attr("id").split("_")[1]) == parseInt(active_rules[i])){
-    //             existe = true;
-    //         }
-    //     }
-    //
-    //
-    //     console.log(exists)
-    //
-    //     if(!exists){
-    //         active_rules.push(parseInt($(this).attr("id").split("_")[1]));
-    //     }
-    //
-    //     localStorage.setItem("rules", JSON.stringify(active_rules));
-    //
-    //     //console.log(localStorage.getItem("rules"));
-    //
-    // })
+     $('.switch').find("input").change(function(){
+         var value = $(this).prop("checked");
+         var id = $(this).attr("id").split("_")[1];
+
+         if(localStorage.getItem("rules") == undefined){
+             localStorage.setItem("rules", JSON.stringify([]))
+         }
+
+         var active_rules = JSON.parse(localStorage.getItem("rules"));
+
+         var exists = false;
+         var index = -1;
+         var rule_this = parseInt($(this).attr("id").split("_")[1]);
+
+         for(var i=0; i<active_rules.length; i++){
+             var rule_i = parseInt(active_rules[i]);
+
+             if(rule_i == rule_this){
+                 exists = true;
+                 index = i;
+                 break;
+             }
+         }
+
+         if(exists == false){
+             active_rules.push(parseInt($(this).attr("id").split("_")[1]));
+         }else{
+            active_rules.splice(index, 1);
+         }
+
+         localStorage.setItem("rules", JSON.stringify(active_rules));
+
+     });
 
 });
